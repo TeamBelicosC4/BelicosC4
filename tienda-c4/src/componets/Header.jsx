2187181aebdfb6c5fa7960abd9609eb48d974168
CP. Enuar Emilio Rosales Salazar <enuaros@CGFM.mil.co>
2022-10-25 15:38:28 -0500

    se organiza el proyectto

--- tienda-c4/src/componets/Header.jsx
+++ tienda-c4/src/componets/Header.jsx
@@ -1,33 +1,43 @@
 import React from "react";
 
-
 function Header() {
-
- 
-
   return (
-
     <>
-
-
       <header>
-
-
         <div class="menu">
-          <i class="fas fa-bars"></i>
+          <ul className="carrito">
+            <li class="submenu">
+              <i class="fas fa-bars"></i>
+              <div id="carrito">
+                <table id="lista-carrito" class="u-full-width">
+                  <thead>
+                    <tr>
+                      <th>Imagen</th>
+                      <th>Nombre</th>
+                      <th>Precio</th>
+                      <th>Cantidad</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody></tbody>
+                </table>
+
+                <a href="#" id="vaciar-carrito" class="button u-full-width">
+                  Vaciar Carrito
+                </a>
+              </div>
+            </li>
+          </ul>
         </div>
 
-
-
-
-        <h1 class="nombre-sitio">Muebles <span>BAUZA </span> </h1>
-
+        <h1 class="nombre-sitio">
+          Muebles <span>BAUZA </span>{" "}
+        </h1>
 
         <ul className="carrito">
           <li class="submenu">
             <img src="/img/cart.png" alt="carrito" />
             <div id="carrito">
-
               <table id="lista-carrito" class="u-full-width">
                 <thead>
                   <tr>
@@ -41,27 +51,14 @@ function Header() {
                 <tbody></tbody>
               </table>
 
-              <a href="#" id="vaciar-carrito" class="button u-full-width">Vaciar Carrito</a>
+              <a href="#" id="vaciar-carrito" class="button u-full-width">
+                Vaciar Carrito
+              </a>
             </div>
           </li>
         </ul>
-
-
       </header>
-
-
-
-
-
     </>
-
-
-
-
-
-
-
-
   );
 }
 
